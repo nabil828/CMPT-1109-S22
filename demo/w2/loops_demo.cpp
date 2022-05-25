cd6b8c9c6628d31420479b6079157bcfcc87a63a
Nabil M. Al-Rousan <rousani@gmail.com>
2022-05-25 13:30:21 -0800

    for, while, do while

---
+++ demo/w2/loops_demo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // for(int i = 5; i < 9; i++){
+    //     cout << i << endl;
+    // }
+
+    // int j = 5;
+    // while(j < 9){
+    //     cout << j << endl;
+    //     j++;
+    // }
+
+    int k = 5;
+    do {
+        cout << k << endl;
+
+        k++;
+    }while(k < 9);
+    
+    return 0;
+}
