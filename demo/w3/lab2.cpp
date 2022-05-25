745c3cc3e7461565f8f6eff5d656e8422a5e1714
Nabil M. Al-Rousan <rousani@gmail.com>
2022-05-25 13:53:38 -0800

    lab2

---
+++ demo/w3/lab2.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int x;
+
+    do
+    {
+
+        int a;
+        int b;
+        int c;
+        int d;
+        double x1, x2;
+        
+        cout <<" a >> b >> c" << endl;
+        
+        cin >> a >> b >> c;
+        if (a == 0)
+        {
+            cout << "Not quadratic \n";
+            if (b == 0)
+            {
+                cout << "a = 0 \n";
+                cout << "b = 0\n";
+                cout << "Bye!\n";
+            }
+            else
+            {
+                x1 = -c / b;
+
+                cout << "x1 = " << x1 << endl;
+            }
+        }
+        else
+        {
+            d = b * b - 4 * a * c;
+
+            if (d < 0)
+            {
+                cout << "No real roots";
+            }
+            else
+            {
+                x1 = (-1 * b + sqrt(d)) / (2 * a);
+
+                x2 = (-1 * b - sqrt(d)) / (2 * a);
+
+                cout << "x1 " << x1 << endl;
+                cout << "x2 " << x2 << endl;
+            }
+        }
+
+        cout << "Please enter your choice" << endl;
+        cout << "1 - To solve a quadratic equations" << endl;
+        cout << " Anything else to exit" << endl;
+        cin >> x;
+    } while (x == 1);
+
+    cout << "Bye!" << endl;
+    return 0;
+}
