3292dc97d6364f09523969b9ff5e3196dd3d7e15
Nabil M. Al-Rousan <rousani@gmail.com>
2022-06-06 12:49:40 -0800

    Call by Reference example

--- demo/w5/call_by_reference.cpp
+++ demo/w5/call_by_reference.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-void f1(int a){
+void f1(int & a){
     a = a + 1; // 11
 }
 
 int main (){
     int a = 10;
-    f1(a); // call by value
+    f1( a); // call by value
 
     cout << endl;
     cout << a; // ?
