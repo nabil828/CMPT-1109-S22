d333ffe685f139d6d567b52822f72eb3f354abd0
Nabil M. Al-Rousan <rousani@gmail.com>
2022-06-06 14:04:47 -0800

    two-dimensional array

--- demo/w5/call_by_reference.cpp
+++ demo/w5/call_by_reference.cpp
@@ -2,24 +2,12 @@
 
 using namespace std;
 
-
-
 int main()
 {
+    int arr[2][3];
+    for (int i = 0; i < 2; i++)
+        for (int j = 0; j < 3; j++)
+            cin >> arr[i][j];
 
-    const int x = 6;
-    int grades[x];
-    grades[0] = 99;
-    grades[1] = 50;
-    grades[2] = 60;
-    grades[3] = 40;
-    grades[4] = 20;
-    grades[5] = 87;
-
-    // cout << sum_(grades) << endl;
-    // cout << min_(grades) << endl;
-    // cout << max_(grades) << endl;
-    // f1(grades);
-    cout << grades[0]; // 99  or 100 ? call-by-reference
     return 0;
 }
