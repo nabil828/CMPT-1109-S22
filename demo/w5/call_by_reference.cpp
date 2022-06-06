85880f93f5bff4e5c48d085bc07ccde59ce0fbe9
Nabil M. Al-Rousan <rousani@gmail.com>
2022-06-06 13:55:09 -0800

    two-dimensional array

--- demo/w5/call_by_reference.cpp
+++ demo/w5/call_by_reference.cpp
@@ -2,45 +2,8 @@
 
 using namespace std;
 
-int sum_(int arr[]){
-    int sum = 0;
-
-    for (int i = 0; i < 6; i++)
-    {
-        sum += arr[i];
-    }
-
-    return sum;
-}
-
-int min_(int arr[]){
-    int min = INT_MAX;
-    
-    for (int i = 0; i < 6; i++)
-    {
-        if (arr[i] < min)
-            min = arr[i];
-    }
-
-    return min;
-}
 
 
-int max_(int arr[]){
-    int max = INT_MIN;
-
-    for (int i = 0; i < 6; i++)
-    {
-        if (arr[i] > max)
-            max = arr[i];
-    }
-
-    return max;
-}
-
-void f1(int arr[]){
-    arr[0] = 100;
-}
 int main()
 {
 
@@ -56,7 +19,7 @@ int main()
     // cout << sum_(grades) << endl;
     // cout << min_(grades) << endl;
     // cout << max_(grades) << endl;
-    f1(grades);
-    cout << grades[0]; // 99  or 100 ? call-by-reference 
+    // f1(grades);
+    cout << grades[0]; // 99  or 100 ? call-by-reference
     return 0;
 }
