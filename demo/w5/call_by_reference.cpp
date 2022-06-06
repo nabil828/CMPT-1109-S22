7d17ebd24205feced2ef36dc22b9bab03172c2cc
Nabil M. Al-Rousan <rousani@gmail.com>
2022-06-06 13:46:56 -0800

    arrays are passed by reference by default

--- demo/w5/call_by_reference.cpp
+++ demo/w5/call_by_reference.cpp
@@ -38,6 +38,9 @@ int max_(int arr[]){
     return max;
 }
 
+void f1(int arr[]){
+    arr[0] = 100;
+}
 int main()
 {
 
@@ -50,8 +53,10 @@ int main()
     grades[4] = 20;
     grades[5] = 87;
 
-    cout << sum_(grades) << endl;
-    cout << min_(grades) << endl;
-    cout << max_(grades) << endl;
+    // cout << sum_(grades) << endl;
+    // cout << min_(grades) << endl;
+    // cout << max_(grades) << endl;
+    f1(grades);
+    cout << grades[0]; // 99  or 100 ? call-by-reference 
     return 0;
 }
