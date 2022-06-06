07674203ea8b68b4d05c5a3b875da1e74e1b6041
Nabil M. Al-Rousan <rousani@gmail.com>
2022-06-06 13:04:39 -0800

    Arrays Example

--- demo/w5/call_by_reference.cpp
+++ demo/w5/call_by_reference.cpp
@@ -2,16 +2,29 @@
 
 using namespace std;
 
-void f1(int & a){
+void f1(int &a)
+{
     a = a + 1; // 11
 }
 
-int main (){
-    int a = 10;
-    f1( a); // call by value
+int main()
+{
+    // int a = 99;
 
-    cout << endl;
-    cout << a; // ?
+    // int b = 80;
+    // int c = 87;
+    const int x = 6;
+    int grades[x];
+    grades[0] = 99;
+    grades[1] = 80;
+    // ...
+    grades[5] = 87;
+    // cout << grades[1];
+    for (int i = 0; i < 6; i++)
+        cin >> grades[i];
+    for (int i = 0; i < 6; i++)
+        cout << grades[i] << endl;
 
+    // x = 10;
     return 0;
 }
