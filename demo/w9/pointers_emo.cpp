1d3297874fd687fcb0da66ad265ad17938611a63
Nabil M. Al-Rousan <rousani@gmail.com>
2022-07-04 13:13:01 -0800

    Update pointers_emo.cpp

--- demo/w9/pointers_emo.cpp
+++ demo/w9/pointers_emo.cpp
@@ -3,15 +3,18 @@ using namespace std;
 
 int main (){
     int x = 5;
-    int *p;
-    p = &x;
+    int *p1;
+    int *p2;
+    
+    // p1 = x; // illegall
+    
+    p1 = &x;
+    p2 = p1; // legal
+    // cout << x << *p1 << *p2;
+    cout << &p1 << " " << &p2;
+    // p2 = &p1; // illegal
 
-    // cout << *p; // 5
 
-    int **p2;
-    p2 = & p;
-
-    cout << **p2;
-
+    &p1 = x;
     return -9923;
 }
