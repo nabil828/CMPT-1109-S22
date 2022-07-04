4d93061de28ace6b9f255a428f9b8ec4bc41c375
Nabil M. Al-Rousan <rousani@gmail.com>
2022-07-04 13:19:24 -0800

    Dynamic Variable

--- demo/w9/pointers_emo.cpp
+++ demo/w9/pointers_emo.cpp
@@ -3,18 +3,9 @@ using namespace std;
 
 int main (){
     int x = 5;
-    int *p1;
-    int *p2;
     
-    // p1 = x; // illegall
-    
-    p1 = &x;
-    p2 = p1; // legal
-    // cout << x << *p1 << *p2;
-    cout << &p1 << " " << &p2;
-    // p2 = &p1; // illegal
-
-
-    &p1 = x;
+    int *p = new int;
+    *p = 10;
+    cout << *p;
     return -9923;
 }
