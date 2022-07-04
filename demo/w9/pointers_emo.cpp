55e8af77773fd262bfdc09efbf03a69c65ba14e5
Nabil M. Al-Rousan <rousani@gmail.com>
2022-07-04 13:36:27 -0800

    Dynamic Arrays

--- demo/w9/pointers_emo.cpp
+++ demo/w9/pointers_emo.cpp
@@ -2,10 +2,26 @@
 using namespace std;
 
 int main (){
-    int x = 5;
-    
-    int *p = new int;
-    *p = 10;
-    cout << *p;
+
+    // int arr [3];
+    // arr[0] = 1;
+    // arr[1] = 2;
+    // arr[2] = 3;
+    // // cout << arr;
+    // int *p = arr;
+    // cout << *(p+2);
+
+    // for(int i = 0  ;i < 3; i++)
+    //     cout << arr[i];// cout << *(arr+i); //cout << p[i]; // cout << *(p+i);
+
+    // Dynamic Arrays
+    int x;
+    cin >> x;
+    int *p = new int [x];
+    cin >> p[0];
+    cin >> *(p+1);
+
+    cout << p[1];
+
     return -9923;
 }
