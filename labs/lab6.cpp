82c63fae219dfdf1ab6d8800188e37ebb4d7c970
Nabil M. Al-Rousan <rousani@gmail.com>
2022-07-06 13:29:49 -0800

    Sign in done

--- labs/lab6.cpp
+++ labs/lab6.cpp
@@ -3,41 +3,73 @@
 using namespace std;
 
 const int ROWS = 4;
-int COLUMNS_PER_ROW_ARR [ROWS] = {5, 6, 4, 3};
-int main () {
-    int **arr = new int *[ROWS];
-    arr[0] = new int [5];
-    arr[1] = new int [6];
-    arr[2] = new int [4];
-    arr[3] = new int [3];
+int COLUMNS_PER_ROW_ARR[ROWS] = {5, 6, 4, 3};
+int **arr = new int *[ROWS];
 
-    int  user_id = 49193;
-    int machine = 2;
-    int lab = 3;
+void display();
 
-    // simulate a login operation
-    arr[lab - 1][machine - 1] = user_id;
+int main()
+{
 
-    for (int i = 0 ; i < ROWS ; i++ ){
-        for (int j = 0; j < COLUMNS_PER_ROW_ARR[i]; j++){
-            cout << arr[i][j] << " ";
+    arr[0] = new int[5];
+    arr[1] = new int[6];
+    arr[2] = new int[4];
+    arr[3] = new int[3];
+
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLUMNS_PER_ROW_ARR[i]; j++)
+        {
+            arr[i][j] = 0;
         }
         cout << endl;
     }
 
+    int selection = -7;
+    do
+    {
+        cout << "choose one of the following" << endl;
+        cout << "1 - Login " << endl;
+        cout << "2- Log off " << endl;
+        cout << "3- Search" << endl;
+        cout << "4- Exist" << endl;
 
+        cin >> selection;
+        switch (selection)
+        {
+        case 1:
+        {
+
+            int user_id = 49193;
+            int machine = 2;
+            int lab = 3;
+            cout << "enter id, lab, & machine " << endl;
+            cin >> user_id >> machine >> lab;
+            // simulate a login operation
+            arr[lab - 1][machine - 1] = user_id;
+            display();
+            break;
+        }
+        default:
+            break;
+        }
+
+    } while (selection != 4);
 
     return 223;
+}
 
-
-    
-    // int *p = new int;
-    // *p= 5;
-
-    // int *p2 ;
-    // p2 = p;
-
-    
-    // delete p;
-    // cout << *p2;
+void display()
+{
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLUMNS_PER_ROW_ARR[i]; j++)
+        {
+            if (arr[i][j] == 0)
+                cout << "empty ";
+            else
+                cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
