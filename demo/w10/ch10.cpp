8340015f6181a4178ba3c0690ab5789f3e23e49e
Nabil M. Al-Rousan <rousani@gmail.com>
2022-07-11 14:15:03 -0800

    inhertiance

--- demo/w10/ch10.cpp
+++ demo/w10/ch10.cpp
@@ -1,66 +1,30 @@
 #include <iostream>
 
 using namespace std;
-
-class Car
-{
-// private:
-    // public: // accessor [private/public/protected]
-    int year; // private  data member
-    string model; // private  data member
-    string make; // private  data member
-
-public:
-    // default constructor 
-    Car(){
-        year = 2022;
-        model = "X";
-        make = "Nissan1";
-    }
-     // constructor 
-    Car(int a){
-        year = a;
-        model = "X";
-        make = "Nissan2";
-    }
-
-    // member function
-    void set_year(int year)
-    {
-        this->year = year;
-    }
-
-    // member function
-    int get_year()
-    {
-        return year;
-    }
-    
-    // member function
-    string get_make()
-    {
-        return make;
-    }
-
-    
-
+class User{
+    public:
+    string name;
+    string DoB;
 };
 
-void f1(Car & aCar){
-    aCar.set_year(2011);
-}
+class Student: public User{
+    public:
+    int GPA;
+};
+
+class Employee: public User{
+    public:
+    string SIN;
+};
 
 int main()
 {
-    Car x;
-    x.set_year(2012);
-    f1(x); // call by reference
-    cout << x.get_year(); // 2011
-
-    Car *p1;
-    p1 = &x;
-    // cout << (*p1).get_year();
-    cout << p1->get_year(); // 2011
-
+    Employee x;
+    x.name = "Nabil";
+    x.SIN = "1212355";
+    Student y;
+    y.name = "Aim";
+    y.GPA = 100;
+    
     return -10;
 }
