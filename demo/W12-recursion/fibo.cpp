7f11368553669506833e2630927d7aa4493945f8
Nabil M. Al-Rousan <rousani@gmail.com>
2022-07-27 13:30:04 -0800

    Update fibo.cpp

--- demo/W12-recursion/fibo.cpp
+++ demo/W12-recursion/fibo.cpp
@@ -2,12 +2,21 @@
 
 using namespace std;
 
-int fib(int n)
+int fib(const int n)
 {
-    if (n == 0 || n == 1)
-        return n;
-    else
-        return fib(n - 1) + fib(n - 2);
+    // if (n == 0 || n == 1)
+    //     return n;
+    // else
+    //     return fib(n - 1) + fib(n - 2);
+    
+    int arr[100];
+    arr[0] = 0;
+    arr[1] = 1;
+
+    for (int i = 2; i <= n ; i++){
+        arr [i] = arr[i-1] + arr[i-2];
+    }
+    return arr[n];
 }
 
 int main()
